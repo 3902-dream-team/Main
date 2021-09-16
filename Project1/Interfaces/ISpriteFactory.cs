8d5744432dcf3d535660ca64c727f22538dee8b5
schlys <samchlystek20@gmail.com>
2021-09-15 22:51:43 -0400

    Added ISpriteFactory

---
+++ Project1/Interfaces/ISpriteFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project1.Interfaces
+{
+    interface ISpriteFactory
+    {
+        private static readonly ISpriteFactory instance;
+        ISpriteFactory Instance { get; }
+        void LoadAllTextures(ContentManager content);
+    }
+}
