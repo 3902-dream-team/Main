e04b103b8a981ba783d360d5602e1763b3d29463
schlys <samchlystek20@gmail.com>
2021-09-15 22:20:24 -0400

    add IContoller and ICommand interfaces

---
+++ Project1/Interfaces/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project1.Interfaces
+{
+    interface ICommand
+    {
+        Game1 game { get; set; }
+
+        void Execute();
+    }
+}
---
+++ Project1/Interfaces/IController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project1.Interfaces
+{
+    interface IController
+    {
+        void RegisterCommand();
+        void Update();
+    }
+}
