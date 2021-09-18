bf7c61a1b4fd4b60d4dae633f1c7cc3b886e599c
Elise Kosmides <kosmides.3@buckeyemail.osu.edu>
2021-09-18 12:45:46 -0400

    enter placeholders for key commands in Game1, create skeleton class for GameCommands to house restart and end.

--- Project1/Command/LinkDirectionCommands.cs
+++ Project1/Command/LinkDirectionCommands.cs
@@ -81,4 +81,24 @@ namespace Project1.Command
             game.Link.MoveLeft();
         }
     }
+
+    class LinkSwordAttackCmd : ICommand
+    {
+        private Game1 game;
+        public Game1 Game
+        {
+            get { return game; }
+            set { game = value; }
+        }
+
+        public LinkSwordAttackCmd(Game1 game)
+        {
+            this.game = game;
+        }
+        public void Execute()
+        {
+            // need to implement 
+            // game.Link.AttackSword(); 
+        }
+    }
 }
--- Project1/Game1.cs
+++ Project1/Game1.cs
@@ -39,6 +39,34 @@ namespace Project1
             keyboard.RegisterCommand(new LinkMoveDownCmd(this), Keys.Down);
             keyboard.RegisterCommand(new LinkMoveRightCmd(this), Keys.Right);
             keyboard.RegisterCommand(new LinkMoveLeftCmd(this), Keys.Left);
+
+            /* Requirement - The 'z' and 'n' key should cause Link to attack using his sword. 
+             * Number keys(1, 2, 3, etc.) should be used to have Link use a different item(later 
+             *  this will be replaced with a menu system and 'x' and 'm' for the secondary item. 
+             * Use 'e' to cause Link to become damaged
+            */
+
+            /* Requirement - Use keys "t" and "y" to cycle between which block is currently being 
+             * shown (i.e. think of the obstacles as being in a list where the game's current 
+             * obstacle is being drawn, "t" switches to the previous item and "y" switches to the next)
+             */
+
+            /* Requirement - Use keys "u" and "i" to cycle between which item is currently being shown 
+             * (i.e. think of the items as being in a list where the game's current item is being drawn, 
+             * "u" switches to the previous item and "i" switches to the next)
+             * Items should move and animate as they do in the final game, but should not interact with any other objects
+             */
+
+            /* Requirement - Use keys "o" and "p" to cycle between which enemy or npc is currently being shown 
+             * (i.e. think of these characters as being in a list where the game's current character is being drawn, 
+             * "o" switches to the previous item and "p" switches to the next)
+             * characters should move, animate, fire projectiles, etc. as they do in the final game, but should not 
+             * interact with any other objects
+             */
+
+            /* Requirement - Use 'q' to quit 
+             * and 'r' to reset the program back to its initial state.
+             */
         }
 
         protected override void Update(GameTime gameTime)
