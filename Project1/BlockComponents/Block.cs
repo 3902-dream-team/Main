30e0297c795bfd96ed8265706001a11f7f3fc195
Chaeun Hong <hong.930@osu.edu>
2021-09-19 16:41:56 -0400

    Edit Block.cs.

--- Project1/BlockComponents/Block.cs
+++ Project1/BlockComponents/Block.cs
@@ -8,20 +8,20 @@ namespace Project1.BlockComponents
 {
     class Block : IBlock
     {
-        private Vector2 position = new Vector2(0, 0);
+        private Vector2 position;
 
         public Block(Vector2 position)
         {
             this.position = position;
         }
 
-        public void Draw()
+        public void Draw(SpriteBatch spriteBatch)
         {
-            Texture2D texture;
-            Rectangle sourceRectangle;
+            //Texture2D texture;
+            Rectangle sourceRectangle = new Rectangle(983, 10, 16, 16);
             Rectangle destinationRectangle = new Rectangle((int)position.X, (int)position.Y, 16, 16);
 
-
+            //spriteBatch.Draw(texture, sourceRectangle, destinationRectangle, Color.White);
         }
     }
 }
