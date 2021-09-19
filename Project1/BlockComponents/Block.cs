7a1563212f91e7b58d8224112db962560ed4be0f
Chaeun Hong <hong.930@osu.edu>
2021-09-19 16:28:48 -0400

    Add Block.png and edit Block.cs.

--- Project1/BlockComponents/Block.cs
+++ Project1/BlockComponents/Block.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Project1.BlockComponents
 {
     class Block : IBlock
     {
+        private Vector2 position = new Vector2(0, 0);
+
+        public Block(Vector2 position)
+        {
+            this.position = position;
+        }
+
         public void Draw()
         {
+            Texture2D texture;
+            Rectangle sourceRectangle;
+            Rectangle destinationRectangle = new Rectangle((int)position.X, (int)position.Y, 16, 16);
+
 
         }
     }
