0f7d0455abc4961b0ba43227b61df5884879f54c
Chaeun Hong <hong.930@osu.edu>
2021-09-19 16:18:35 -0400

    Add Block class.

---
+++ Project1/BlockComponents/Block.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project1.BlockComponents
+{
+    class Block : IBlock
+    {
+        public void Draw()
+        {
+
+        }
+    }
+}
